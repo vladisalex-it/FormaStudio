c2ad89ca9f0923bb3a486e6742258d178b218d8f
Vladislav Zakharov <vl4disalex@gmail.com>
2025-09-14 17:07:46 +0300

    Add slider section with integrated logic and adjust layout for responsiveness.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ const menuBtn = document.querySelector('.menu__btn')
 menuBtn.addEventListener('click', function() {
     menuList.classList.toggle('menu-list--active')
 })
+
+
+
+const swiper = new Swiper('.projects__slider', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+        640: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+
+
+  navigation: {
+    nextEl: '.projects__btn-next',
+    prevEl: '.projects__btn-prev',
+  },
+  
+});
